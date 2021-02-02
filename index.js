dd905ab3c4a077f52f892ac2325ff36c3f2aae99
Amneet <harryamneet600@gmail.com>
2021-02-02 14:45:40 +0530

    Simple chat

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
-
+let i = 0;
+var socket = require('socket.io');
 // appp setup
 var app = express();
 var server = app.listen(4000, function() {
@@ -8,3 +9,18 @@ var server = app.listen(4000, function() {
 
 // static files
 app.use(express.static('public'));
+
+
+// socket setup
+// this paratmeter takes hte server that we are working with
+var io = socket(server);
+
+// when particular client connect , it makes a socket connection
+io.on('connection', function(socket) {
+    socket.on('chat message', (msg) => {
+        console.log(i);
+        io.emit('chat message', msg);
+        ++i;
+
+    });
+})
